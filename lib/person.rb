abe27b2d8f8181ed938fc879a2ab0ce037e2f7cb
brian ambundo <brian.katiechi9@gmail.com>
2022-08-22 12:36:32 +0300

    commit tree 0dec58f83164999fcc92c2d7b35bd7c9bf7b7cca

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+    def initialize (name)
+        @name = name
+    end
+    def bank_account(value)
+        self.bank_account = value
+    end
+    
+end
+
+stella = Person.new('Blake')
+stella.bank_account(25)
+return stella.bank_account
+
